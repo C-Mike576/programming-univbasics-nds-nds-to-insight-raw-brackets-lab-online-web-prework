6ca3af7fb980fa195fc69ddd27d1227b09c67b07
Michael Calkins <m.calkins576@gmail.com>
2019-10-20 19:19:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -31,9 +31,10 @@ def directors_totals(nds)
         movie_gross += nds[name_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
-    result[nds[name_index][:name]] = movie_gross
-    name_index += 1
-    pp result
+      if result.
+      result[nds[name_index][:name]] = movie_gross
+      name_index += 1
+      pp result
   end
 
 end
